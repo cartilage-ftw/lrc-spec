ce4e14f2540ca7bf53a232cbe598d143d0dc000b
Aayush Arya <tomriddle257@gmail.com>
2024-07-21 15:21:57 +0200

    add option to display lab notes summary

--- main/launch_gui.py
+++ main/launch_gui.py
@@ -11,10 +11,12 @@ import saturation_curve
 from bokeh.plotting import figure
 from bokeh.models import Span
 
+from pathlib import Path
 
+# set title page and favicon displayed on the web app
 st.set_page_config(page_title='LRC Analysis Pipeline', page_icon=':stars:')
 
-# display logo
+# I want to show the LRC logo at the top
 lrc_logo_url = 'https://www.lrc-project.eu/' + \
                 r'Webpage%20of%20the%20European%20LRC%20project-Dateien/LRC-color-300x77.png'
 
@@ -33,24 +35,76 @@ Start of GUI buttons, etc.
 
 file_type = st.radio("Content of File:", ['Raw Time Series', 'Extracted Spectrum'])
 
+
 if 'file_modified' not in st.session_state:
-    st.session_state.file_modified = False
+    st.session_state.file_modified = True
+# initialize a variable stored in the session state, which is accessible globally, in a safe way.
+if 'file_name' not in st.session_state:
+    st.session_state.file_name = None
+if 'file_bytes_data' not in st.session_state:
+    st.session_state.file_bytes_data = None
+if 'buncher_delay' not in st.session_state:
+    st.session_state.buncher_delay = None
+if 'bunching_freq' not in st.session_state:
+    st.session_state.bunching_freq = None
 
 def handle_file_change():
-    print("FILE MODIFIED!")
-    st.session_state.file_modified = True
-    
+    """
+    This method is invoked each time the file uploader is touched, or the session
+    is refreshed
+
+    NOTE: After uploading a file, it will remain in memory for a while before being wiped out.
+       => Make sure that the routine that reads data from this file isn't being called
+       over and over, but only when the file is changed; This ensures you don't get "Broken Pipe"
+        errors because the file is no longer there.
+    """
+    print("Checking file change", time.time())
+    # make sure the variable at least exists.
+    if 'uploaded_file' in globals():
+        print("THIS TEST PASSED")
+        # check if the file has likely changed
+        if uploaded_file.name != st.session_state.file_name:
+            print("FILE NAME CHANGED!")
+            st.session_state.file_name = uploaded_file.name
+        
+        # To see if the uploaded file has *actually* changed, it's better to compare the bytes data
+        # instead of just the file name as the file with the same name can get modified locally
+        if True:#uploaded_file.getvalue() != st.session_state.file_bytes_data:
+            # take action based on loaded file type (raw time series or spectrum)
+            prepared = True
+            if 'raw' in file_type.lower():
+                print("CHECKING IF PREPARED".lower())
+                if st.session_state.buncher_delay == None or st.session_state.bunching_freq == None:
+                    prepared = False
+                if prepared == True:
+                    # update the global variable that contains the loaded series
+                    st.session_state.atd = load_time_series(uploaded_file)
+            elif 'spectrum' in file_type.lower():
+                # TODO: call the method to load spectrum here.
+                pass
+            else:
+                st.write("ERROR! Did you change the name of the specified file types?\n" + \
+                         "The type could not be identified")
+            # update the stored file bytes data
+            st.session_state.file_bytes_data = uploaded_file.getvalue()
+
+
 uploaded_file = st.file_uploader("Enter the file you'd like to read from", on_change=handle_file_change())
 
-
 def plot_spectrum_from_file(file):
+    """
+    When the file type specified is an extracted "spectrum" which can be directly displayed
+    """
     st.write('Reading file:', file.name)
     global spec_data
     spec_data = pd.read_csv(file)
-    plot_spectrum_from_data(spec_data, file.name)
+    plot_spectrum_from_data(spec_data)
 
 
 def plot_spectrum_from_data(spec_data):
+    """
+    A more generic method for displaying the spectrum, when the 
+    """
     global spec_fig
     spec_fig = visual_utils.make_spectrum_fig(spec_data)
     st.bokeh_chart(spec_fig)
@@ -76,6 +130,7 @@ def display_atd(atd, wavenum_req, wavenum_obs='#TODO'):
 
 st.write(st.session_state)
 
+# TODO: This info should be displayed only when loading raw time series.
 st.write("""In some measurements you may have kept the laser off,
           in which case the wavemeter read out a garbage value such as -33333""")
 
@@ -83,47 +138,50 @@ garbage_wave_flag = st.radio("""Discard recorded hits with garbage wavelength re
                             options=['Yes', 'No, keep'])
 
 
-def load_atd():
-    st.session_state.file_name=uploaded_file.name
-    return io_utils.read_lrc_timeseries(uploaded_file, buncher_delay, 1/bunching_freq,
-                         discard_garbage=(garbage_wave_flag == 'Yes'))
+def load_time_series(uploaded_file):
+    return io_utils.read_lrc_timeseries(uploaded_file,
+                                         st.session_state.buncher_delay,
+                                         1/st.session_state.bunching_freq,
+                                         discard_garbage=(garbage_wave_flag == 'Yes'))
 
 
 if 'display_spectrum' not in st.session_state:
     st.session_state.display_spectrum = True
 
-
+if 'atd' not in st.session_state:
+    st.session_state.atd = None
 #def toggle_spectrum_display():
 #st.session_state.display_spectrum = True#not st.session_state.display_spectrum
 
 
 if uploaded_file is not None:
+    handle_file_change()
     if 'spectrum' in file_type.lower():
         try:
+            handle_file_change()
             plot_spectrum_from_file(uploaded_file)
         except Exception as e:
-            st.write(f"Error plotting spectrum from file {uploaded_file.name}! " + \
-                  "\nPlease check if you're using the correct file/option")
+            st.write(f"Error plotting spectrum from file {uploaded_file.name}!\n" + \
+                  "Please check if you're using the correct file/option")
             st.write(e)
     else:
         try:
             st.write("### Arrival Time Distribution")
-            bunching_freq = st.number_input('Bunching Freq (in Hz)', value=100)
-            buncher_delay = st.number_input('Buncher Delay (in seconds)')
-
-            if st.session_state.file_modified == True:
-                global atd
-                atd = load_atd()
-                st.session_state.file_modified = False
-
-            if 'atd' in globals():
+            st.session_state.bunching_freq = st.number_input('Bunching Freq (in Hz)', value=100)
+            st.session_state.buncher_delay = st.number_input('Buncher Delay (in seconds)')
+            print("CHECKING FILE CHANGE!")
+            handle_file_change()
+            print("CHECKING IF ATD is DEFINED")
+            if st.session_state.atd is not None:
+                atd = st.session_state.atd
+                print("WAS THIS REACHED?")
                 display_wavenum = st.select_slider("Select wavenumber step to display",
-                                            options=list(dict.fromkeys(atd['wavenum_req'])))
+                                        options=list(dict.fromkeys(st.session_state.atd['wavenum_req'])))
                 st.write("Current setting at", display_wavenum*2, 'second harmonic. Please mind that ' + \
                                     "there may be an offset between requested and actual. " + \
                                         "Trust the WS7 wavemeter readout instead.")
                 ms_cut_pos = st.slider('GS Cutoff [ms]', value=0.295, step=1E-3, format='%.3f')
-                display_atd(atd, wavenum_req=display_wavenum)
+                display_atd(st.session_state.atd, wavenum_req=display_wavenum)
             
             filter1 = 0.#st.number_input('OD 1', value=0.)
             filter2 = 0.#st.number_input('OD 2', value=0.)
@@ -132,10 +190,9 @@ if uploaded_file is not None:
         except Exception as e:
             st.write(f'Error plotting the ATD from {uploaded_file.name}!\n' +
                   'Please check if all wavenumbers are not garbage (e.g. -33333)')
-            st.write(e.__str__)
             st.write(e)
-            if atd is not None:
-                st.write(atd)
+            if st.session_state.atd is not None:
+                st.write(st.session_state.atd)
             else:
                 st.write("WARNING: Instance of `atd` has disappeared")
 
@@ -143,7 +200,7 @@ if uploaded_file is not None:
 if uploaded_file is not None and st.session_state.display_spectrum == True:
     try:
         st.write("Creating spectrum")
-        if 'atd' in globals():
+        if st.session_state.atd is not None:
             spectrum_data = io_utils.make_spectrum(atd, ms_cut=ms_cut_pos, filters=[filter1, filter2],
                                 transm_percent=saturation_curve.get_transm(filter1, filter2),
                                 save_file=False)
@@ -230,3 +287,19 @@ if fit_button:
                          vary_gauss_component=(gaussian_fixed_flag == 'Variable')))
     
     #st.write(f"""{fit.fit_report()}""")
+
+
+
+"""
+______
+## Lab Notebook Summary
+"""
+
+show_meas_notes = st.radio("Do you want to look at the notes from your past measurements?",
+                           options=['Yes, please', 'No'])
+
+notes_file_path = '../notes/measurements/summary_key_measurements.md'
+
+if 'yes' in show_meas_notes.lower():
+    md_file_contents = Path(notes_file_path).read_text()
+    st.write(md_file_contents)
